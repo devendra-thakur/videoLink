8eca0031a7bd5e51a24564d3697897d521b2adc2
devendra-thakur <44664741+devendra-thakur@users.noreply.github.com>
2024-07-23 17:40:21 +0530

    test
    tset

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuthTest
+//
+//  Created by Devendra Thakur on 29/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
